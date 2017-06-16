62c85ec05edd77e0cd87937aaafe597b6f16ad57
Joonwoo Park <joonwoop@codeaurora.org>
2017-06-16 11:58:58 -0700

    sched: introduce sched_{up,down}migrate knobs
    Introduce knobs sched_upmigrate and sched_downmigrate to
    control thresholds to up and down migrate tasks between clusters at a
    runtime for ease of power and performance tuning.
    
    Change-Id: I17a464388b404b61d9a9dbe1beee4883c4db8e3f
    Signed-off-by: Joonwoo Park <joonwoop@codeaurora.org>
    [satyap@codeaurora.org: resolve merge conflicts & update
    functinality as needed on msm-4.14]
    Signed-off-by: Satya Durga Srinivasu Prabhala <satyap@codeaurora.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -150,6 +150,9 @@ unsigned int sysctl_sched_cfs_bandwidth_slice		= 5000UL;
  */
 unsigned int capacity_margin				= 1280;
 
+unsigned int sysctl_sched_capacity_margin_up = 1078; /* ~5% margin */
+unsigned int sysctl_sched_capacity_margin_down = 1205; /* ~15% margin */
+
 static inline void update_load_add(struct load_weight *lw, unsigned long inc)
 {
 	lw->weight += inc;
@@ -3664,7 +3667,8 @@ static inline unsigned long cfs_rq_load_avg(struct cfs_rq *cfs_rq)
 
 static int idle_balance(struct rq *this_rq, struct rq_flags *rf);
 
-static inline int task_fits_capacity(struct task_struct *p, long capacity);
+static inline int task_fits_capacity(struct task_struct *p, long capacity,
+									 int cpu);
 
 static inline void update_misfit_status(struct task_struct *p, struct rq *rq)
 {
@@ -3676,7 +3680,7 @@ static inline void update_misfit_status(struct task_struct *p, struct rq *rq)
 		return;
 	}
 
-	if (task_fits_capacity(p, capacity_of(cpu_of(rq)))) {
+	if (task_fits_capacity(p, capacity_of(cpu_of(rq)), rq->cpu)) {
 		rq->misfit_task_load = 0;
 		return;
 	}
@@ -7389,9 +7393,17 @@ static int select_idle_sibling(struct task_struct *p, int prev, int target)
 	return select_idle_sibling_cstate_aware(p, prev, target);
 }
 
-static inline int task_fits_capacity(struct task_struct *p, long capacity)
+static inline int task_fits_capacity(struct task_struct *p, long capacity,
+									 int cpu)
 {
-	return capacity * 1024 > boosted_task_util(p) * capacity_margin;
+	unsigned int margin;
+
+	if (capacity_orig_of(task_cpu(p)) > capacity_orig_of(cpu))
+		margin = sysctl_sched_capacity_margin_down;
+	else
+		margin = sysctl_sched_capacity_margin_up;
+
+	return capacity * 1024 > boosted_task_util(p) * margin;
 }
 
 static int start_cpu(bool boosted)
@@ -7744,7 +7756,7 @@ static int wake_cap(struct task_struct *p, int cpu, int prev_cpu)
 	/* Bring task utilization in sync with prev_cpu */
 	sync_entity_load_avg(&p->se);
 
-	return !task_fits_capacity(p, min_cap);
+	return !task_fits_capacity(p, min_cap, cpu);
 }
 
 static bool cpu_overutilized(int cpu)
