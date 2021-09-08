3ce281105e9c55035fd1c96d639fd71818f30fc0
Kazuki Hashimoto <kaz205@tuta.io>
2021-09-09 01:05:45 +0900

    sde: Speed up sde_kms_commit
    This function is the code responsible for sending frames to the display.
    Speed it up by forcing the code to be executed on a big CPU and
    restricting that CPU to shallow idle states.
    
    Signed-off-by: Kazuki Hashimoto <kaz205@tuta.io>

--- drivers/gpu/drm/msm/sde/sde_kms.c
+++ drivers/gpu/drm/msm/sde/sde_kms.c
@@ -27,6 +27,8 @@
 #include <linux/memblock.h>
 #include <linux/bootmem.h>
 #include <linux/msm_drm_notify.h>
+#include <linux/pm_qos.h>
+#include <linux/sched.h>
 
 #include "msm_drv.h"
 #include "msm_mmu.h"
@@ -1113,6 +1115,11 @@ static void sde_kms_commit(struct msm_kms *kms,
 	struct sde_kms *sde_kms;
 	struct drm_crtc *crtc;
 	struct drm_crtc_state *old_crtc_state;
+	struct pm_qos_request req = {
+		.type = PM_QOS_REQ_AFFINE_CORES,
+		.cpus_affine = ATOMIC_INIT(BIT(smp_processor_id()))
+	};
+	cpumask_t old_mask;
 	int i;
 
 	if (!kms || !old_state)
@@ -1124,6 +1131,8 @@ static void sde_kms_commit(struct msm_kms *kms,
 		return;
 	}
 
+	sched_migrate_to_cpumask_start(&old_mask, cpu_perf_mask);
+	pm_qos_add_request(&req, PM_QOS_CPU_DMA_LATENCY, 100);
 	SDE_ATRACE_BEGIN("sde_kms_commit");
 	for_each_crtc_in_state(old_state, crtc, old_crtc_state, i) {
 		if (crtc->state->active) {
@@ -1133,6 +1142,8 @@ static void sde_kms_commit(struct msm_kms *kms,
 	}
 
 	SDE_ATRACE_END("sde_kms_commit");
+	pm_qos_remove_request(&req);
+	sched_migrate_to_cpumask_end(&old_mask, cpu_perf_mask);
 }
 
 void sde_kms_release_splash_resource(struct sde_kms *sde_kms,
