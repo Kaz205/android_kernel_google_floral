83e8228e3f97c830e449827bacc38b47803ce05c
Kazuki Hashimoto <kaz205@tuta.io>
2021-09-14 04:11:10 +0900

    sched/tune: Stub out prefer_high_cap
    Signed-off-by: Kazuki Hashimoto <kaz205@tuta.io>

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -485,6 +485,18 @@ boost_write(struct cgroup_subsys_state *css, struct cftype *cft,
 	return 0;
 }
 
+static u64 prefer_high_cap_read(struct cgroup_subsys_state *css,
+				struct cftype *cft)
+{
+	return 0;
+}
+
+static int prefer_high_cap_write(struct cgroup_subsys_state *css,
+				 struct cftype *cft, u64 prefer_high_cap)
+{
+	return 0;
+}
+
 static struct cftype files[] = {
 	{
 		.name = "boost",
@@ -495,6 +507,11 @@ static struct cftype files[] = {
 		.name = "prefer_idle",
 		.read_u64 = prefer_idle_read,
 		.write_u64 = prefer_idle_write,
+	},
+	{
+		.name = "prefer_high_cap",
+		.read_u64 = prefer_high_cap_read,
+		.write_u64 = prefer_high_cap_write,
 	},
 	{ }	/* terminate */
 };
